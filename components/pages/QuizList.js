3602598245626e3f2f8fbfc53d01f9ecf0f6811e
Eri <trgianghuynhdev1808@gmail.com>
2021-06-08 18:13:29 +0700

    fix: err

--- components/pages/QuizList.js
+++ components/pages/QuizList.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Text, View, StyleSheet, ScrollView } from "react-native";
-import { Portal, Modal, Button } from 'react-native-paper';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { Avatar, Card, Title, Paragraph } from 'react-native-paper';
+import { Portal, Modal, Button } from "react-native-paper";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { Avatar, Card, Title, Paragraph } from "react-native-paper";
 import { useState } from "react";
 import { useEffect } from "react";
 import {
@@ -14,18 +14,22 @@ import {
 
 const QuizList = ({ navigation, route }) => {
   const [visible, setVisible] = useState(false);
-  function showModal() { setVisible(true); }
-  function hideModal() { setVisible(false); }
+  function showModal() {
+    setVisible(true);
+  }
+  function hideModal() {
+    setVisible(false);
+  }
 
   const containerStyle = {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     padding: 20,
-    margin: 20
+    margin: 20,
   };
 
   useEffect(() => {
     if (route.params && route.params.receiveResult) {
-      showModal()
+      showModal();
     }
   }, [route]);
 
@@ -80,28 +84,45 @@ const QuizList = ({ navigation, route }) => {
       done: 0,
       total: 16,
     },
-  ])
+  ]);
 
   return (
     <View style={{ backgroundColor: "#ffffff", height: "100%" }}>
       <Portal style={{ padding: 0, margin: 0 }}>
-        <Modal visible={visible} onDismiss={hideModal} style={{ padding: 0, margin: 0 }} contentContainerStyle={containerStyle}>
-          <MaterialCommunityIcons name="check-circle-outline" style={{ color: "#4CAF50", fontSize: 50, textAlign: 'center' }} />
-          <Text style={{ fontSize: 18, textAlign: 'center', marginTop: 10, }}>Bạn đã  hoàn thành bài trắc nghiệm với số câu đúng là 14/16.</Text>
-          <Button style={{ backgroundColor: "#4CAF50", marginTop: 15 }} color="white" labelStyle={{ fontSize: 17 }} onPress={hideModal} onPress={() => {navigation.navigate("QuizResult", {quizLession: "Nguyên tử"}); setVisible(false) }}>Xem kết quả</Button>
+        <Modal
+          visible={visible}
+          onDismiss={hideModal}
+          style={{ padding: 0, margin: 0 }}
+          contentContainerStyle={containerStyle}
+        >
+          <MaterialCommunityIcons
+            name="check-circle-outline"
+            style={{ color: "#4CAF50", fontSize: 50, textAlign: "center" }}
+          />
+          <Text style={{ fontSize: 18, textAlign: "center", marginTop: 10 }}>
+            Bạn đã hoàn thành bài trắc nghiệm với số câu đúng là 14/16.
+          </Text>
+          <Button
+            style={{ backgroundColor: "#4CAF50", marginTop: 15 }}
+            color="white"
+            labelStyle={{ fontSize: 17 }}
+            onPress={hideModal}
+            onPress={() => {
+              navigation.navigate("QuizResult", { quizLession: "Nguyên tử" });
+              setVisible(false);
+            }}
+          >
+            Xem kết quả
+          </Button>
         </Modal>
       </Portal>
 
       <View style={styles.breadcum}>
-        <Text style={styles.breadcumText}>
-          Bài học
-        </Text>
+        <Text style={styles.breadcumText}>Bài học</Text>
 
         <Icon name="chevron-right" style={styles.chevronIcon} />
 
-        <Text style={styles.breadcumText}>
-          Hóa học 10
-        </Text>
+        <Text style={styles.breadcumText}>Hóa học 10</Text>
 
         <Icon name="chevron-right" style={styles.chevronIcon} />
 
@@ -111,26 +132,45 @@ const QuizList = ({ navigation, route }) => {
       </View>
 
       <ScrollView style={styles.items}>
-        {
-          lessions.map((lession, index) => (
-            <Card style={[styles.cardStyle, index === lessions.length - 1 ? {marginBottom: 20} : {}]} onPress={() => navigation.navigate("Quiz", {quizLession: lession.name})}>
-              <Card.Content>
-                <View style={styles.cardContentInfoWrapper}>
-                  <Text style={styles.cardContentInfo}>{lession.name}</Text>
+        {lessions.map((lession, index) => (
+          <Card
+            style={[
+              styles.cardStyle,
+              index === lessions.length - 1 ? { marginBottom: 20 } : {},
+            ]}
+            onPress={() =>
+              navigation.navigate("Quiz", { quizLession: lession.name })
+            }
+          >
+            <Card.Content>
+              <View style={styles.cardContentInfoWrapper}>
+                <Text style={styles.cardContentInfo}>{lession.name}</Text>
+              </View>
+              <View style={styles.cardContentProgressWrapper}>
+                <View style={styles.progressBarWrapper}>
+                  <View
+                    style={[
+                      styles.activeProgressBar,
+                      ,
+                      { width: `${(lession.done / lession.total) * 100}%` },
+                    ]}
+                  ></View>
+                  <View
+                    style={[
+                      styles.nonActiveProgressBar,
+                      { width: `${(1 - lession.done / lession.total) * 100}%` },
+                    ]}
+                  ></View>
                 </View>
-                <View style={styles.cardContentProgressWrapper}>
-                  <View style={styles.progressBarWrapper}>
-                    <View style={[styles.activeProgressBar, , { width: `${lession.done/lession.total * 100}%` }]}></View>
-                    <View style={[styles.nonActiveProgressBar, { width: `${(1 - lession.done/lession.total) * 100}%` }]}></View>
-                  </View>
-                  <View>
-                    <Text style={{ fontWeight: '400' }}>{lession.done}/{lession.total}</Text>
-                  </View>
+                <View>
+                  <Text style={{ fontWeight: "400" }}>
+                    {lession.done}/{lession.total}
+                  </Text>
                 </View>
-              </Card.Content>
-            </Card>
-          ))
-        }
+              </View>
+            </Card.Content>
+          </Card>
+        ))}
       </ScrollView>
     </View>
   );
@@ -145,12 +185,12 @@ const styles = StyleSheet.create({
     margin: 10,
     fontSize: 16,
     borderRadius: 4,
-    backgroundColor: "rgba(33, 33, 33, 0.08)"
+    backgroundColor: "rgba(33, 33, 33, 0.08)",
   },
 
   breadcumText: {
     color: "rgba(0, 0, 0, 0.5)",
-    fontSize: 18
+    fontSize: 18,
   },
 
   activeBreadcumText: {
@@ -183,7 +223,7 @@ const styles = StyleSheet.create({
 
   cardStyle: {
     marginTop: 20,
-    width: '100%',
+    width: "100%",
     shadowColor: "#000",
     borderLeftWidth: 4,
     borderLeftColor: "#004d04",
@@ -192,45 +232,45 @@ const styles = StyleSheet.create({
       height: 5,
     },
     shadowOpacity: 0.39,
-    shadowRadius: 8.30,
+    shadowRadius: 8.3,
   },
 
   cardContentInfo: {
     fontSize: 18,
     fontWeight: "600",
-    color: '#004d04'
+    color: "#004d04",
   },
 
   cardContentInfoWrapper: {
-    borderBottomColor: '#004d04',
+    borderBottomColor: "#004d04",
     paddingBottom: 15,
-    borderBottomWidth: 1
+    borderBottomWidth: 1,
   },
 
   activeProgressBar: {
     height: 5,
-    backgroundColor: '#004d04'
+    backgroundColor: "#004d04",
   },
 
   nonActiveProgressBar: {
     height: 5,
-    backgroundColor: "rgba(33, 33, 33, 0.1)"
+    backgroundColor: "rgba(33, 33, 33, 0.1)",
   },
 
   progressBarWrapper: {
-    display: 'flex',
-    flexDirection: 'row',
+    display: "flex",
+    flexDirection: "row",
     // marginTop: 15,
-    width: "85%"
+    width: "85%",
   },
 
   cardContentProgressWrapper: {
     paddingTop: 6,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  }
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
 });
 
 export default QuizList;
--- components/pages/QuizResult.js
+++ components/pages/QuizResult.js
@@ -1,189 +1,337 @@
 import React from "react";
 import { TextInput, ScrollView, Text, View, StyleSheet } from "react-native";
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { Modal, Portal, FAB, RadioButton, Button, DefaultTheme, ThemeProvider as PaperProvider } from 'react-native-paper';
-import { useState } from "react";
+import Icon from "react-native-vector-icons/FontAwesome";
 import {
-	MaterialIcons
-} from "@expo/vector-icons";
+  Modal,
+  Portal,
+  FAB,
+  RadioButton,
+  Button,
+  DefaultTheme,
+  ThemeProvider as PaperProvider,
+} from "react-native-paper";
+import { useState } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
 const result = [
-	{ status: 'correct' },
-	{ status: 'correct' },
-	{ status: 'correct' },
-	{ status: 'correct' },
+  { status: "correct" },
+  { status: "correct" },
+  { status: "correct" },
+  { status: "correct" },
 
-	{ status: 'correct' },
-	{ status: 'incorrect' },
-	{ status: 'correct' },
-	{ status: 'correct' },
+  { status: "correct" },
+  { status: "incorrect" },
+  { status: "correct" },
+  { status: "correct" },
 
-	{ status: 'correct' },
-	{ status: 'inactive' },
-	{ status: 'correct' },
-	{ status: 'correct' },
+  { status: "correct" },
+  { status: "inactive" },
+  { status: "correct" },
+  { status: "correct" },
 
-	{ status: 'correct' },
-	{ status: 'correct' },
-	{ status: 'correct' },
-	{ status: 'correct' },
-]
+  { status: "correct" },
+  { status: "correct" },
+  { status: "correct" },
+  { status: "correct" },
+];
 
 const bgEnum = {
-	'correct': "#B2DFDB",
-	'incorrect': "#BB1010",
-	'inactive': "rgba(33, 33, 33, 0.1)"
-}
+  correct: "#B2DFDB",
+  incorrect: "#BB1010",
+  inactive: "rgba(33, 33, 33, 0.1)",
+};
 
 const colorEnum = {
-	'correct': "#000000",
-	'incorrect': "#fff",
-	'inactive': "#000000",
-}
+  correct: "#000000",
+  incorrect: "#fff",
+  inactive: "#000000",
+};
 
 const QuizResult = ({ navigation, route }) => {
-	const theme = {
-		...DefaultTheme,
-		colors: {
-			...DefaultTheme.colors,
-			primary: "#004640",
-			accent: "#f1c40f",
-		},
-	};
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: "#004640",
+      accent: "#f1c40f",
+    },
+  };
 
-	function navigateToAnswerScreen(idx) {
-		navigation.navigate("QuizAnswer", { quizLession: "Nguyên tử", qIdx: idx })
-	}
+  function navigateToAnswerScreen(idx) {
+    navigation.navigate("QuizAnswer", { quizLession: "Nguyên tử", qIdx: idx });
+  }
 
+  return (
+    <PaperProvider theme={theme}>
+      <ScrollView
+        style={{ backgroundColor: "#ffffff", height: "100%", display: "flex" }}
+      >
+        <View style={styles.breadcum}>
+          <Text style={styles.breadcumText}>Bài tập</Text>
 
-	return (
-		<PaperProvider theme={theme}>
-			<View style={{ backgroundColor: "#ffffff", height: "100%", display: 'flex' }}>
-				<View style={styles.breadcum}>
+          <Icon name="chevron-right" style={styles.chevronIcon} />
 
-					<Text style={styles.breadcumText}>
-						Bài tập
+          <Text style={styles.breadcumText}>Kết quả</Text>
+
+          <Icon name="chevron-right" style={styles.chevronIcon} />
+
+          <Text style={[styles.breadcumText, styles.activeBreadcumText]}>
+            {route.params.quizLession.length > 30
+              ? route.params.quizLession.substring(0, 27) + "..."
+              : route.params.quizLession}
           </Text>
+        </View>
 
-					<Icon name="chevron-right" style={styles.chevronIcon} />
+        <View style={styles.mainSection}>
+          <View
+            style={{
+              backgroundColor: "#B2DFDB",
+              borderColor: "#B2DFDB",
+              paddingHorizontal: 10,
+              paddingVertical: 15,
+              fontWeight: "500",
+              borderRadius: 4,
+              borderWidth: 0,
+              marginBottom: 15,
+              marginTop: 10,
+            }}
+          >
+            <Text style={{ fontSize: 20 }}>Số câu đúng: 14/16</Text>
+          </View>
 
-					<Text style={styles.breadcumText}>
-						Kết quả
-          </Text>
+          <View
+            style={{
+              backgroundColor: "rgba(33, 33, 33, 0.1)",
+              borderColor: "rgba(33, 33, 33, 0.1)",
+              paddingHorizontal: 10,
+              paddingVertical: 15,
+              fontWeight: "500",
+              borderRadius: 4,
+              borderWidth: 0,
+              marginBottom: 15,
+            }}
+          >
+            <Text style={{ fontSize: 20 }}>Số câu bỏ qua: 1/16</Text>
+          </View>
 
-					<Icon name="chevron-right" style={styles.chevronIcon} />
+          <View
+            style={{
+              backgroundColor: "#BB1010",
+              borderColor: "#BB1010",
+              paddingHorizontal: 10,
+              paddingVertical: 15,
+              fontWeight: "500",
+              borderRadius: 4,
+              borderWidth: 0,
+              marginBottom: 15,
+            }}
+          >
+            <Text style={{ color: "#fff", fontSize: 20 }}>
+              Số câu sai: 1/16
+            </Text>
+          </View>
 
-					<Text style={[styles.breadcumText, styles.activeBreadcumText]}>
-						{route.params.quizLession.length > 30 ? route.params.quizLession.substring(0, 27) + '...' : route.params.quizLession}
-					</Text>
-				</View>
+          <View>
+            <Text
+              style={{
+                color: "#000",
+                textAlign: "right",
+                fontSize: 18,
+                fontWeight: "500",
+                marginBottom: 15,
+              }}
+            >
+              Đã làm vào 19:30 ngày 7/6/2021
+            </Text>
+          </View>
+        </View>
 
-				<View style={styles.mainSection}>
-					<View style={{ backgroundColor: '#B2DFDB', borderColor: "#B2DFDB", paddingHorizontal: 10, paddingVertical: 15, fontWeight: "500", borderRadius: 4, borderWidth: 0, marginBottom: 15, marginTop: 10 }}>
-						<Text style={{ fontSize: 20 }}>Số câu đúng: 14/16</Text>
-					</View>
+        <View style={{ paddingHorizontal: 10 }}>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              borderWidth: 1,
+              borderColor: "#004d40",
+              padding: 10,
+              borderTopLeftRadius: 4,
+              borderTopRightRadius: 4,
+              borderBottomWidth: 0,
+            }}
+          >
+            {result.slice(0, 7).map((elm, idx) => (
+              <View
+                style={{
+                  width: 46,
+                  height: 46,
+                  backgroundColor: bgEnum[elm.status],
+                  borderRadius: 46 / 2,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text
+                  onPress={() => navigateToAnswerScreen(idx)}
+                  style={{ fontSize: 18, color: colorEnum[elm.status] }}
+                >
+                  {idx + 1}
+                </Text>
+              </View>
+            ))}
+          </View>
 
-					<View style={{ backgroundColor: 'rgba(33, 33, 33, 0.1)', borderColor: "rgba(33, 33, 33, 0.1)", paddingHorizontal: 10, paddingVertical: 15, fontWeight: "500", borderRadius: 4, borderWidth: 0, marginBottom: 15 }}>
-						<Text style={{ fontSize: 20 }}>Số câu bỏ qua: 1/16</Text>
-					</View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              borderWidth: 1,
+              borderLeftWidth: 1,
+              borderRightWidth: 1,
+              borderColor: "#004d40",
+              padding: 10,
+            }}
+          >
+            {result.slice(7, 14).map((elm, idx) => (
+              <View
+                style={{
+                  width: 46,
+                  height: 46,
+                  backgroundColor: bgEnum[elm.status],
+                  borderRadius: 46 / 2,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text
+                  onPress={() => navigateToAnswerScreen(idx + 7)}
+                  style={{ fontSize: 18, color: colorEnum[elm.status] }}
+                >
+                  {idx + 8}
+                </Text>
+              </View>
+            ))}
+          </View>
 
-					<View style={{ backgroundColor: '#BB1010', borderColor: "#BB1010", paddingHorizontal: 10, paddingVertical: 15, fontWeight: "500", borderRadius: 4, borderWidth: 0, marginBottom: 15 }}>
-						<Text style={{ color: '#fff', fontSize: 20 }}>Số câu sai: 1/16</Text>
-					</View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              marginTop: 0,
+              borderWidth: 1,
+              borderColor: "#004d40",
+              padding: 10,
+              borderBottomRightRadius: 4,
+              borderBottomLeftRadius: 4,
+              borderTopWidth: 0,
+            }}
+          >
+            {result.slice(14, 16).map((elm, idx) => (
+              <View
+                style={{
+                  width: 46,
+                  height: 46,
+                  backgroundColor: bgEnum[elm.status],
+                  borderRadius: 46 / 2,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginRight: 12,
+                }}
+              >
+                <Text
+                  onPress={() => navigateToAnswerScreen(idx + 14)}
+                  style={{ fontSize: 18, color: colorEnum[elm.status] }}
+                >
+                  {idx + 15}
+                </Text>
+              </View>
+            ))}
+          </View>
+        </View>
 
-					<View>
-						<Text style={{color: '#000', textAlign: 'right', fontSize: 18, fontWeight: "500", marginBottom: 15}}>Đã làm vào 19:30 ngày 7/6/2021</Text>
-					</View>
-				</View>
-
-				<View style={{ paddingHorizontal: 10 }}>
-					<View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', borderWidth: 1, borderColor: "#004d40", padding: 10, borderTopLeftRadius: 4, borderTopRightRadius: 4, borderBottomWidth: 0 }}>
-						{
-							result.slice(0, 7).map((elm, idx) =>
-							(
-								<View style={{ width: 46, height: 46, backgroundColor: bgEnum[elm.status], borderRadius: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-									<Text onPress={() => navigateToAnswerScreen(idx)} style={{ fontSize: 18, color: colorEnum[elm.status] }}>{idx + 1}</Text>
-								</View>
-							)
-							)
-						}
-					</View>
-
-					<View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', borderWidth: 1, borderLeftWidth: 1, borderRightWidth: 1, borderColor: "#004d40", padding: 10 }}>
-						{
-							result.slice(7, 14).map((elm, idx) =>
-							(
-								<View style={{ width: 46, height: 46, backgroundColor: bgEnum[elm.status], borderRadius: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-									<Text onPress={() => navigateToAnswerScreen(idx+7)} style={{ fontSize: 18, color: colorEnum[elm.status] }}>{idx + 8}</Text>
-								</View>
-							)
-							)
-						}
-					</View>
-
-					<View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', marginTop: 0, borderWidth: 1, borderColor: "#004d40", padding: 10, borderBottomRightRadius: 4, borderBottomLeftRadius: 4, borderTopWidth: 0 }}>
-						{
-							result.slice(14, 16).map((elm, idx) =>
-							(
-								<View style={{ width: 46, height: 46, backgroundColor: bgEnum[elm.status], borderRadius: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center', marginRight: 12 }}>
-									<Text onPress={() => navigateToAnswerScreen(idx+14)} style={{ fontSize: 18, color: colorEnum[elm.status] }}>{idx + 15}</Text>
-								</View>
-							)
-							)
-						}
-					</View>
-				</View>
-
-				<View style={{ marginTop: 'auto', paddingHorizontal: 10 }}>
-					<View style={{display: "flex", flexDirection: "row", alignItems: "center", marginBottom: 15, borderWidth: 1, borderColor: '#004640', padding: 10, borderRadius: 4}}>
-						<MaterialIcons name="info" color="#004640" size={20} />
-						<Text style={{marginLeft: 10, fontSize: 17, fontWeight: "500", color: "#004640"}}>Bạn phải đạt >= 80% mới hoàn thành bài tập</Text>
-					</View>
-					<Button style={{ marginBottom: 20, backgroundColor: '#004640' }} labelStyle={{ fontSize: 19, color: '#fff' }} onPress={() => navigation.navigate("Quiz", {quizLession: "Nguyên tử"})} >Làm lại</Button>
-				</View>
-			</View>
-		</PaperProvider>
-	);
+        <View style={{ paddingHorizontal: 10, marginTop: 20 }}>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              marginBottom: 15,
+              borderWidth: 1,
+              borderColor: "#004640",
+              padding: 10,
+              borderRadius: 4,
+            }}
+          >
+            <MaterialIcons name="info" color="#004640" size={20} />
+            <Text
+              style={{
+                marginLeft: 10,
+                fontSize: 17,
+                fontWeight: "500",
+                color: "#004640",
+              }}
+            >
+              Bạn phải đạt >= 80% mới hoàn thành bài tập
+            </Text>
+          </View>
+          <Button
+            style={{ marginBottom: 20, backgroundColor: "#004640" }}
+            labelStyle={{ fontSize: 19, color: "#fff" }}
+            onPress={() =>
+              navigation.navigate("Quiz", { quizLession: "Nguyên tử" })
+            }
+          >
+            Làm lại
+          </Button>
+        </View>
+      </ScrollView>
+    </PaperProvider>
+  );
 };
 
 const styles = StyleSheet.create({
-	breadcum: {
-		display: "flex",
-		flexDirection: "row",
-		justifyContent: "flex-start",
-		padding: 10,
-		margin: 10,
-		fontSize: 16,
-		borderRadius: 4,
-		backgroundColor: "rgba(33, 33, 33, 0.08)"
-	},
+  breadcum: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    padding: 10,
+    margin: 10,
+    fontSize: 16,
+    borderRadius: 4,
+    backgroundColor: "rgba(33, 33, 33, 0.08)",
+  },
 
-	breadcumText: {
-		color: "rgba(0, 0, 0, 0.5)",
-		fontSize: 18
-	},
+  breadcumText: {
+    color: "rgba(0, 0, 0, 0.5)",
+    fontSize: 18,
+  },
 
-	activeBreadcumText: {
-		color: "rgb(0, 0, 0)",
-	},
+  activeBreadcumText: {
+    color: "rgb(0, 0, 0)",
+  },
 
-	chevronIcon: {
-		fontSize: 15,
-		color: "rgba(0, 0, 0, 0.2)",
-		marginTop: 4,
-		marginHorizontal: 10,
-	},
+  chevronIcon: {
+    fontSize: 15,
+    color: "rgba(0, 0, 0, 0.2)",
+    marginTop: 4,
+    marginHorizontal: 10,
+  },
 
-	mainSection: {
-		width: '100%',
-		paddingHorizontal: 10
-	},
+  mainSection: {
+    width: "100%",
+    paddingHorizontal: 10,
+  },
 
-	mainCard: {
-		// fontSize: "18px",
-		// border
-		borderWidth: 1,
-		borderRadius: 4
-	}
+  mainCard: {
+    // fontSize: "18px",
+    // border
+    borderWidth: 1,
+    borderRadius: 4,
+  },
 });
 
 // "rgba(33, 33, 33, 0.1)"
